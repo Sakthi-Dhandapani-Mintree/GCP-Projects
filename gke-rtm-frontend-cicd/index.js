50c532eade1f96bae4e33a6616cdf3691acbfc4f
Sakthi-Dhandapani-Mintree <43699383+Sakthi-Dhandapani-Mintree@users.noreply.github.com>
2018-11-09 12:31:41 +0530

    Update index.js

--- gke-rtm-frontend-cicd/index.js
+++ gke-rtm-frontend-cicd/index.js
@@ -1,32 +1,19 @@
-//1.
-var http = require('http');
-var fs = require('fs');
+
+
+var express = require('express');
+var path = require('path');
 var app = express();
-app.use(express.static(__dirname));
-//2.
-var server = http.createServer(function (req, resp) {
-     //3.
-    if (req.url === "/") {
-        fs.readFile("index.html", function (error, pgResp) {
-           if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write(pgResp);
-            }
-             
-            resp.end();
-        });
-    } else {
-        //4.
-        resp.writeHead(200, { 'Content-Type': 'text/html' });
-        resp.write(req.url);
-        resp.end();
-    }
-    app.get('ResourceMapping.js', function(req, res) {
-    res.sendFile(path.join(__dirname + '/ResourceMapping.js'));
+app.use(express.static(__dirname + 'client'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client/index.html'));
 });
+
+app.get('/client/client.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client.js'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 //5.
 server.listen(8090);
