df9abf7777759483b6c0e44f63bc4a0a469834f9
Sakthi-Dhandapani-Mintree <43699383+Sakthi-Dhandapani-Mintree@users.noreply.github.com>
2018-11-09 15:36:43 +0530

    Update ResourceBeanController.java

--- gke-rtm-backend-cicd/resourcetechnologymapping/src/main/java/com/resoursetechmapping/controller/ResourceBeanController.java
+++ gke-rtm-backend-cicd/resourcetechnologymapping/src/main/java/com/resoursetechmapping/controller/ResourceBeanController.java
@@ -32,7 +32,7 @@ public class ResourceBeanController {
 	/**
 	 * This method used insert the resource into Database it wont save into database 
 	 */
-	@RequestMapping(value = "/helloji", method = RequestMethod.GET)
+	@RequestMapping(value = "/hellokube", method = RequestMethod.GET)
 	public String hello() {
 		System.out.println("Hello My method is called");
 		return "My hello Method called";
