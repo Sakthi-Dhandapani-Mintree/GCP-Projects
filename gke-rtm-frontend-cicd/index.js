1df964e731ee8965f4f83d3eb64d48c131905d7a
Sakthi-Dhandapani-Mintree <43699383+Sakthi-Dhandapani-Mintree@users.noreply.github.com>
2018-11-09 09:33:59 +0530

    Update index.js

--- gke-rtm-frontend-cicd/index.js
+++ gke-rtm-frontend-cicd/index.js
@@ -1,6 +1,20 @@
 //1.
 var http = require('http');
 var fs = require('fs');
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " path/to/directory");
+    process.exit(-1);
+}
+ 
+var path = process.argv[2];
+ 
+fs.readdir(path, function(err, items) {
+    console.log(items);
+ 
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+});
 //2.
 var server = http.createServer(function (req, resp) {
      
