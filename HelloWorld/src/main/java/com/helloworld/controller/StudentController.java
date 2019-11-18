d32a257583057300ec404455321589ee2734d12a
Sakthi-Dhandapani-Mintree <43699383+Sakthi-Dhandapani-Mintree@users.noreply.github.com>
2019-11-18 21:23:33 +0530

    Add files via upload

---
+++ HelloWorld/src/main/java/com/helloworld/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping(value="/welcome")
+public class StudentController {
+	@RequestMapping(value = "/hi")
+	public String hi() {
+		System.out.println("Welcome Student Controller");
+		return "Hi Student";
+	}
+
+	@GetMapping(value = "/action")
+	public String action() {
+		return "Student Action";
+	}
+}
