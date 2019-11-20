7bfa81fd59939e9b322c618d4f458a5e5ea67866
Sakthi-Dhandapani-Mintree <43699383+Sakthi-Dhandapani-Mintree@users.noreply.github.com>
2019-11-20 15:27:20 +0530

    Update RatingController.java

--- rating-service/src/main/java/example/RatingController.java
+++ rating-service/src/main/java/example/RatingController.java
@@ -5,15 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@RequestMapping(value = "/rating")
 public class RatingController {
 
     @RequestMapping(value = "/check")
     public String hi() {
-        return "Checking Rating";
+        return "------------------------>Checking Rating <----------------------------";
     }
 
     @GetMapping(value = "/action")
     public String action() {
-        return "Rating Action";
+        return "------------------------>Rating Action <----------------------------";
     }
 }
