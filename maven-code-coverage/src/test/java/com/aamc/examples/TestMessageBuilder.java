223929cfc61ad33fe8ecf6e34750089e86de1922
Sakthi-Dhandapani-Mintree <43699383+Sakthi-Dhandapani-Mintree@users.noreply.github.com>
2019-11-26 20:51:42 +0530

    Add files via upload

---
+++ maven-code-coverage/src/test/java/com/aamc/examples/TestMessageBuilder.java
@@ -0,0 +1,17 @@
+package com.aamc.examples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMessageBuilder {
+
+    @Test
+    public void testNameMkyong() {
+
+        MessageBuilder obj = new MessageBuilder();
+        assertEquals("Hello Sakthi Dhandapani", obj.getMessage("Sakthi"));
+
+    }
+
+ }
